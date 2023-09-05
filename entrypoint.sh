72c172032e0d51726b9418de0a33ef5040859879
Vinh Nguyen <vinh.nglx@gmail.com>
2023-09-05 20:54:02 +1200

    Some enhancements: - add dynamodb-local into dockercompose - update entrypoint script - update missing package

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # TODO: check if commented out line has any impact when ran using Docker
 #flask db upgrade
+python3 -m pip install -r requirements.txt
+python3 scripts/dyndb_create_pm_table.py
+python3 scripts/dyndb_create_users_table.py
 flask run --host=0.0.0.0 --port 5001
